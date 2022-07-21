43fa05eccced84c3b601795603d4c819cdc9feac
namnguyen1994 <namnguyen1994@gmail.com>
2022-07-20 23:10:34 -0400

    new image and restaurant name/header changes

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -7,13 +7,14 @@ import './Header.css';
 const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
-      <SubHeading title="Chase the new flavour"/>
-      <h1 className="app__header-h1">The Key to Fine Dining</h1>
-
+      <SubHeading title="Happy to serve!"/>
+      <h1 className="app__header-h1">The True Joy of Cooking</h1>
+      <p className="p__opensans" style={{margin: '2rem 0'}}> Repeating trial and error and failing many times… it’s that process which makes the dishes shine.</p>
+      <button type="button" className="custom__button"> Explore Menu </button>
     </div>
 
     <div className="app__wrapper_img">
-
+      <img src={images.welcome} alt="header img" />
     </div>
   </div>
 );
