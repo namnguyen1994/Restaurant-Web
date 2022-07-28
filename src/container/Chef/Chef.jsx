fe36c972871b43ea4acc67265f832aac431047cf
namnguyen1994 <namnguyen1994@gmail.com>
2022-07-27 21:42:56 -0400

    Menu Updated

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className="app__bg app__wrapper section__padding">
+    
   </div>
 );
 
